4b6760df5aeae6bb585f5b3f1b918ca0d3d5bb36
deba.ai <dp0000000004@gamil.com>
2026-09-20 20:39:12 +0530

    added token createion for user at end point

--- little_lemon_app/urls.py
+++ little_lemon_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('menu-items/', views.menuItemsViews, name="menu_items"),
     path('menu-items/<int:id>', views.singleMenuViews, name='singleMenu'),
     path('category/', views.catagoryView),
     path('data/', views.hardcodedData),
     path('s-msg/', views.secreate),
+    path('auth_create', obtain_auth_token),
 ]
